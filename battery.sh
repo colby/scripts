43f6b636086f7ecf2a0f6b2c0ffdc484ba42b448
Colby Olson <me@colbyolson.com>
2014-03-06 13:10:09 -0800

    added warning color if below warning percent

--- battery.sh
+++ battery.sh
@@ -3,9 +3,19 @@
 # To include the line below into your .tmux.conf to see battery % or charging icon.
 # set -g status-right "#(/Users/colbyolson/src/scripts/battery.sh) #H %H:%M"
 
-color="#[fg=colour236]"
+ok_color="colour236"
+warn_percent="20%"
+warn_color="colour208"
+
 status=`pmset -g batt`
 percent=`echo $status | cut -d' ' -f7 | cut -d';' -f1`
+
+if [[ $percent < $warn_percent ]]
+    then
+        color="#[fg=$warn_color]"
+    else
+        color="#[fg=$ok_color]"
+fi
 
 if [[ $status == *Battery*Power* ]]
     then
