06ed455808e1427254b4aa46c92ba53826716cca
Colby Olson <me@colbyolson.com>
2014-03-07 13:44:39 -0800

    bash comparisons are dumb, percent_warn now works as it should

--- battery.sh
+++ battery.sh
@@ -4,22 +4,20 @@
 # set -g status-right "#(/Users/colbyolson/src/scripts/battery.sh) #H %H:%M"
 
 ok_color="colour236"
-warn_percent="20%"
+warn_percent="20"
 warn_color="colour208"
 
-status=`pmset -g batt`
-percent=`echo $status | cut -d' ' -f7 | cut -d';' -f1`
+status=$(pmset -g batt)
+percent=$(echo $status | cut -d' ' -f7 | cut -d'%' -f1)
 
-if [ "$percent" \< "$warn_percent" ]
-    then
+if [[ "$percent" -lt "$warn_percent" ]]; then
         color="#[fg=$warn_color]"
     else
         color="#[fg=$ok_color]"
 fi
 
-if [[ $status == *Battery*Power* ]]
-    then
-        printf "%s%s%s" "$color" "$percent" "#[default]"
+if [[ $status == *Battery*Power* ]]; then
+        printf "%s%s%s" "$color" "$percent%" "#[default]"
     else
         printf "%s%s%s" "$color" "⚡" "#[default]"
 fi
