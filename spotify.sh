b192da7af66f0dd22b2e0cbebad785e4a11013fe
Colby Olson <me@colbyolson.com>
2016-05-09 11:58:05 -0700

    show if spotify is paused

--- spotify.sh
+++ spotify.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-# To include the line below into your .tmux.conf to see spotify information
-# set -g status-right "#(/Users/colbyolson/src/scripts/spotify.sh) #H %H:%M"
+# Include the line below into your .tmux.conf to see spotify information
+# set -g status-right "#(~/src/scripts/spotify.sh) #H %H:%M"
 
 color="colour240"
 
-if ps aux | grep '/[A]pplications/Spotify.app/Contents/MacOS/Spotify' > /dev/null; then
+if ps aux | grep '/[A]pplications/Spotify.app/Contents/MacOS/Spotify' > /dev/null
+then
     artist=$(osascript -e 'tell application "Spotify" to artist of current track as string');
     track=$(osascript -e 'tell application "Spotify" to name of current track as string');
+    state=$(osascript -e 'tell application "Spotify" to player state as text')
 
-    printf "%s%s%s" "#[fg=$color]" "♫ $artist - $track" "#[default]"
+    if [ "$state" == "paused" ]
+    then
+        state=" [paused]"
+    else
+        state=""
+    fi
+
+    printf "%s%s%s" "#[fg=$color]" "♫ $artist - $track$state" "#[default]"
 else
     return
 fi
