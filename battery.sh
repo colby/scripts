7a545b61678a4583bc3fd5cc98ccc84d54f6e33a
Colby Olson <me@colbyolson.com>
2016-10-05 14:53:05 -0700

    fix regex, change logic, store status as variable

--- battery.sh
+++ battery.sh
@@ -3,21 +3,25 @@
 # To include the line below into your .tmux.conf to see battery % or charging icon.
 # set -g status-right "#(/Users/colbyolson/src/scripts/battery.sh) #H %H:%M"
 
+# change color when battery level drops below $warning level
+warning=20
+
 ok_color="colour240"
-warning="20"
 warning_color="colour208"
 
 status=$(pmset -g batt)
-percent=$(pmset -g batt | grep -oE '[1-9]{1,3}%')
+percent=$(grep -oE '[0-9]{1,3}%' <<< "$status")
 
-if [[ "$percent" -lt "$warning" ]]; then
-        color="#[fg=$warning_color]"
-    else
-        color="#[fg=$ok_color]"
+if [[ "${percent//%}" -lt "$warning" ]]
+then
+    color="#[fg=$warning_color]"
+else
+    color="#[fg=$ok_color]"
 fi
 
-if [[ $status == *Battery*Power* ]]; then
-        printf "%s%s%s" "$color" "$percent" "#[default]"
-    else
-        printf "%s%s%s" "$color" "⚡" "#[default]"
+if [[ $status == *Battery*Power* ]]
+then
+    printf "%s%s%s" "$color" "$percent" "#[default]"
+else
+    printf "%s%s%s" "$color" "⚡" "#[default]"
 fi
