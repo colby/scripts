3f854421870d33993f982b2119ed3c47bdbcb022
Colby Olson <me@colbyolson.com>
2020-05-21 12:39:01 -0700

    ask for sudo once, then run

--- overlord.sh
+++ overlord.sh
@@ -4,7 +4,9 @@ set -x
 
 which cpulimit || brew install cpulimit
 
+sudo -s -- <<'EOF'
 for process in TaniumCX TaniumClient TaniumEndpointIndex trapsd
 do
     pgrep $process | xargs -I X sudo cpulimit -l 0 -p X &
 done
+EOF
